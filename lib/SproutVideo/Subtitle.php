6bd53f4f66482f7265bde67c7b20cb330f2a2696
David Whitaker <damwhit@gmail.com>
2021-07-20 13:49:47 -0400

    Write all subtitle methods

--- lib/SproutVideo/Subtitle.php
+++ lib/SproutVideo/Subtitle.php
@@ -2,29 +2,29 @@
 namespace SproutVideo;
 class Subtitle extends Resource
 {
-	public static function list_subtitles($options=null)
+	public static function list_subtitles($options)
 	{
 		return self::get(self::build_url($options), $options);
 	}
 
-	public static function get_playlist($playlist_id, $options=null)
+	public static function get_subtitle($options)
 	{
-		return self::get('playlists/'.$playlist_id, $options);
+		return self::get(self::build_url($options).'/'.$options['id'], $options);
 	}
 
-	public static function create_playlist($data, $options=null)
+	public static function create_subtitle($data, $options)
 	{
-		return self::post('playlists', $data, $options);
+		return self::post(self::build_url($options), $data, $options);
 	}
 
-	public static function update_playlist($playlist_id, $data, $options=null)
+	public static function update_subtitle($data, $options)
 	{
-		return self::put('playlists/'.$playlist_id, $data, $options);
+		return self::put(self::build_url($options).'/'.$options['id'], $data, $options);
 	}
 
-	public static function delete_playlist($playlist_id, $options=null)
+	public static function delete_subtitle($options)
 	{
-		return self::delete('playlists/'.$playlist_id, $options);
+		return self::delete(self::build_url($options).'/'.$options['id'], $options);
 	}
 
     private static function build_url($options)
